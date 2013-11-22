5392772acd3307b2fc0b834ad8e4fa4316211e30
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-22 12:24:04 -0800

    Add event to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ActiveRecord::Base
   belongs_to :located, :polymorphic => true
+  belongs_to :event, :foreign_key => :located_id, :foreign_type => 'Event'
+
   geocoded_by :address
   before_validation :geocode, :if => :changed?
 end
