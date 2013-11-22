5e8e50a1eba4a71347f5fd584e2695ec34b6fd79
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-22 10:49:36 -0800

    Brand new rails

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeocoderPolymorphismExample::Application.config.session_store :cookie_store, key: '_Geocoder-Polymorphism-Example_session'
