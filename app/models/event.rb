aa18efdbda68758faa51e82f18da1fbba9a9bcd0
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-22 13:03:19 -0800

    Awesome scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   has_one :location, as: :located, dependent: :destroy
   accepts_nested_attributes_for :location
+
+  scope :price_over, lambda{|p| where('events.price >= ?', p) }
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,10 @@
 class Location < ActiveRecord::Base
   belongs_to :located, :polymorphic => true
-  belongs_to :event, :foreign_key => :located_id, :foreign_type => 'Event'
+
+  def self.filtered_by type 
+    belongs_to type, :foreign_key => :located_id, :foreign_type => type.to_s.capitalize
+    where(:located_type => type.to_s.capitalize).joins(type).includes(:located)
+  end
 
   geocoded_by :address
   before_validation :geocode, :if => :changed?
