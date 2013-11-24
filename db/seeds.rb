294ec629c5358888d885ebd9c0dca4fd6a0b5722
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-24 13:00:56 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Venue.create(name: 'House', location_attributes: {address: '2295 Harrison St San Francisco, CA 94110'})
+Venue.create(name: 'Work', location_attributes: {address: '3126 16th St. San Francisco, CA 94103'})
+Venue.create(name: 'Gym', location_attributes: {address: '1200 Van Ness Ave San Francisco, CA 94109'})
+
+Event.create(price: 10, location_attributes: {address: '2295 Harrison St San Francisco, CA 94110'})
+Event.create(price: 20, location_attributes: {address: '3126 16th St. San Francisco, CA 94103'})
+Event.create(price: 5, location_attributes: {address: '1200 Van Ness Ave San Francisco, CA 94109'})
